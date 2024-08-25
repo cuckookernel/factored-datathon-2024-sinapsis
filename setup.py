1cc098888ae0300636744090f15578a7f35c185c
cuckookernel <cuckookernel>
2024-08-25 14:26:52 +0000

    remove deps from setup.py

--- setup.py
+++ setup.py
@@ -9,19 +9,7 @@ setup(
     author='Team Sinapsis',
     author_email='mr324@cornell.edu',
     packages=['data_proc', "shared"],
-    install_requires=["beautifulsoup4==4.12.3",
-                      "boto3==1.35.4",
-                      "botocore==1.35.4",
-                      "brotlipy==0.7.0",
-                      "click==8.1.7", # CLI construction
-                      "dataset==1.6.2",
-                      "deflate==0.7.0",
-                      "pandas==2.2.2",
-                      "pydantic==2.8.2",
-                      "python-dotenv==1.0.1",
-                      "requests==2.31.0",
-                      "rich==13.7.1",
-                      ],
+    install_requires=[],
 
     classifiers=[
         'Development Status :: 1 - Planning',
